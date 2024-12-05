1b8bc5ce2388350fd020d6931ea5198d47aaba93
Israa Mohamed <saramohamed2315@gmail.com>
2024-12-05 18:09:01 +0200

    Update CPUSchedulersSimulator.java

--- CPUSchedulersSimulator.java
+++ CPUSchedulersSimulator.java
@@ -10,47 +10,123 @@ public class CPUSchedulersSimulator {
         List<Process> processes = new ArrayList<>();
 
         // Input
+        System.out.println("Welcome To CPU Schedulers Simulator ");
+        System.out.println("===================================");
         System.out.println("Enter number of processes: ");
         int n = sc.nextInt();
-        System.out.println("Enter Round Robin Quantum Time: ");
-        int quantumTime = sc.nextInt();
+        // System.out.println("Enter Round Robin Quantum Time: ");
+        // int quantumTime = sc.nextInt();
         System.out.println("Enter Context Switching Time: ");
         int contextSwitchTime = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter details for Process " + (i + 1) + ":");
-            System.out.print("Name: ");
-            String name = sc.next();
-//            System.out.print("Color: ");
-//            String color = sc.next();
-            System.out.print("Arrival Time: ");
-            int arrivalTime = sc.nextInt();
-            System.out.print("Burst Time: ");
-            int burstTime = sc.nextInt();
-            System.out.print("Priority: ");
-            int priority = sc.nextInt();
-            processes.add(new Process(name, "", arrivalTime, burstTime, priority));
-        }
 
         // Execute all scheduling algorithms
 //        simulatePriorityScheduling(processes, contextSwitchTime);
 //        simulateSJF(processes, contextSwitchTime);
 //        simulateSRTF(processes, contextSwitchTime);
 //        simulateFCAIScheduling(processes, quantumTime, contextSwitchTime);
+
         PrioirtyScheduler priorityScheduler = new PrioirtyScheduler();
         SJFScheduler sjfScheduler = new SJFScheduler();
+        FCAIScheduler fc = new FCAIScheduler(processes);
         SRTFScheduler srtfScheduler = new SRTFScheduler();
-        System.out.println("Enter type of schedule you want: ");
-        int type = sc.nextInt();
-        if (type == 1){
-            priorityScheduler.simulatePriorityScheduling(processes,contextSwitchTime);
-        }
-        else if(type == 2)
-            sjfScheduler.simulateSJF(processes,contextSwitchTime);
-        else
-            srtfScheduler.simulateSRTF(processes,contextSwitchTime);
+        while (true) {
+            // Display the menu
+            System.out.println("\n=== Scheduler Menu ===");
+            System.out.println("1. Priority Scheduling");
+            System.out.println("2. SJF Scheduling");
+            System.out.println("3. FCAI Scheduling");
+            System.out.println("4. SRTF Scheduling");
+            System.out.println("5. Exit");
+            // Get user choice
+            System.out.println("Enter type of schedule you want: ");
+            int type = sc.nextInt();
 
-        sc.close();
+            // Execute corresponding scheduling algorithm
+            switch (type) {
+                case 1:
+                for (int i = 0; i < n; i++) {
+                    System.out.println("Enter details for Process " + (i + 1) + ":");
+                    System.out.print("Name: ");
+                    String name = sc.next();
+                    System.out.print("Arrival Time: ");
+                    int arrivalTime = sc.nextInt();
+                    System.out.print("Burst Time: ");
+                    int burstTime = sc.nextInt();
+                    System.out.print("Priority: ");
+                    int priority = sc.nextInt();
+                    processes.add(new Process(name, "", arrivalTime, burstTime, priority,0));
+                }
+                    System.out.println("\nRunning Priority Scheduling...");
+                    priorityScheduler.simulatePriorityScheduling(processes,contextSwitchTime);
+                    break;
+
+                case 2:
+                for (int i = 0; i < n; i++) {
+                    System.out.println("Enter details for Process " + (i + 1) + ":");
+                    System.out.print("Name: ");
+                    String name = sc.next();
+                    System.out.print("Arrival Time: ");
+                    int arrivalTime = sc.nextInt();
+                    System.out.print("Burst Time: ");
+                    int burstTime = sc.nextInt();
+                    System.out.print("Priority: ");
+                    int priority = sc.nextInt();
+                    processes.add(new Process(name, "", arrivalTime, burstTime, priority,0));
+                }
+                    System.out.println("\nRunning SJF Scheduling...");
+                    sjfScheduler.simulateSJF(processes, contextSwitchTime);
+                    break;
+
+                case 3:
+                for (int i = 0; i < n; i++) {
+                    System.out.println("Enter details for Process " + (i + 1) + ":");
+                    System.out.print("Name: ");
+                    String name = sc.next();
+                    System.out.print("Arrival Time: ");
+                    int arrivalTime = sc.nextInt();
+                    System.out.print("Burst Time: ");
+                    int burstTime = sc.nextInt();
+                    System.out.print("Priority: ");
+                    int priority = sc.nextInt();
+                    System.out.println("Enter Round Robin Quantum Time: ");
+                   int  quantumTime = sc.nextInt();
+                    processes.add(new Process(name, "", arrivalTime, burstTime, priority,quantumTime));
+                }
+                    System.out.println("\nRunning FCAI Scheduling...");
+                    fc.simulateFCAIScheduling(processes);
+                    break;
+
+                case 4:
+                for (int i = 0; i < n; i++) {
+                    System.out.println("Enter details for Process " + (i + 1) + ":");
+                    System.out.print("Name: ");
+                    String name = sc.next();
+                    System.out.print("Arrival Time: ");
+                    int arrivalTime = sc.nextInt();
+                    System.out.print("Burst Time: ");
+                    int burstTime = sc.nextInt();
+                    System.out.print("Priority: ");
+                    int priority = sc.nextInt();
+                    processes.add(new Process(name, "", arrivalTime, burstTime, priority,0));
+                }
+                    System.out.println("\nRunning SRTF Scheduling...");
+                    srtfScheduler.simulateSRTF(processes,contextSwitchTime);
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+                    break;
+            }
+        }
+    
+        }
+           
     }
 
     // Non-preemptive Priority Scheduling
@@ -175,4 +251,3 @@ public class CPUSchedulersSimulator {
 //        System.out.println("Average Waiting Time = " + (double) totalWaitingTime / processes.size());
 //        System.out.println("Average Turnaround Time = " + (double) totalTurnaroundTime / processes.size());
 //    }
-}
