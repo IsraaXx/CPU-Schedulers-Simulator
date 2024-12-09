420364dc9dcafa94daadc33c2dd4148d305cde49
Israa Mohamed <saramohamed2315@gmail.com>
2024-12-09 17:23:49 +0200

    Update CPUSchedulersSimulator.java

--- CPUSchedulersSimulator.java
+++ CPUSchedulersSimulator.java
@@ -28,7 +28,7 @@ public class CPUSchedulersSimulator {
 
         PrioirtyScheduler priorityScheduler = new PrioirtyScheduler();
         SJFScheduler sjfScheduler = new SJFScheduler();
-        FCAIScheduler fc = new FCAIScheduler(processes);
+        
         SRTFScheduler srtfScheduler = new SRTFScheduler();
         while (true) {
             // Display the menu
@@ -91,10 +91,17 @@ public class CPUSchedulersSimulator {
                     int priority = sc.nextInt();
                     System.out.println("Enter Round Robin Quantum Time: ");
                    int  quantumTime = sc.nextInt();
-                    processes.add(new Process(name, "", arrivalTime, burstTime, priority,quantumTime));
-                }
+                    processes.add(new Process(name, "", arrivalTime, burstTime, priority,quantumTime)); }
+                    
+                 double v1 = (double) processes.stream().mapToInt(Process::getArrivalTime).max().orElse(1) / 10;
+                 double v2 = (double) processes.stream().mapToInt(Process::getBurstTime).max().orElse(1) / 10;
+                  for(Process p : processes){
+                   double  FCAIFactor = (10 - p.priority) + ((double)p.arrivalTime / v1) + ((double)p.remainingTime / v2);
+                   p.FCAIFactor= (int) Math.ceil(FCAIFactor);
+                  }
+                    FCAIScheduler fc = new FCAIScheduler(processes);
                     System.out.println("\nRunning FCAI Scheduling...");
-                    fc.simulateFCAIScheduling(processes);
+                    fc.simulateFCAIScheduling();
                     break;
 
                 case 4:
@@ -128,126 +135,3 @@ public class CPUSchedulersSimulator {
         }
            
     }
-
-    // Non-preemptive Priority Scheduling
-//    static void simulatePriorityScheduling(List<Process> processes, int contextSwitchTime) {
-//        System.out.println("\n=== Non-Preemptive Priority Scheduling ===");
-//        processes.sort(Comparator.comparingInt((Process p) -> p.priority).thenComparingInt(p -> p.arrivalTime));
-//        simulateNonPreemptive(processes, contextSwitchTime);
-//    }
-
-    // Non-preemptive Shortest Job First (SJF)
-//    static void simulateSJF(List<Process> processes, int contextSwitchTime) {
-//        System.out.println("\n=== Non-Preemptive Shortest Job First (SJF) ===");
-//        processes.sort(Comparator.comparingInt((Process p) -> p.burstTime).thenComparingInt(p -> p.arrivalTime));
-//        simulateNonPreemptive(processes, contextSwitchTime);
-//    }
-
-    // Helper for Non-Preemptive Schedulers
-//    static void simulateNonPreemptive(List<Process> processes, int contextSwitchTime) {
-//        int currentTime = 0, totalWaitingTime = 0, totalTurnaroundTime = 0;
-//
-//        for (Process p : processes) {
-//            int waitingTime = Math.max(0, currentTime - p.arrivalTime);
-//            p.waitingTime = waitingTime;
-//            p.turnaroundTime = waitingTime + p.burstTime;
-//            totalWaitingTime += waitingTime;
-//            totalTurnaroundTime += p.turnaroundTime;
-//
-//            System.out.println("Process " + p.name + ": Waiting Time = " + waitingTime
-//                    + ", Turnaround Time = " + p.turnaroundTime);
-//
-//            currentTime += p.burstTime + contextSwitchTime;
-//        }
-//
-//        System.out.println("Average Waiting Time = " + (double) totalWaitingTime / processes.size());
-//        System.out.println("Average Turnaround Time = " + (double) totalTurnaroundTime / processes.size());
-//    }
-
-    // Shortest Remaining Time First (SRTF)
-//    static void simulateSRTF(List<Process> processes, int contextSwitchTime) {
-//        System.out.println("\n=== Shortest Remaining Time First (SRTF) ===");
-//        int currentTime = 0, completed = 0, totalWaitingTime = 0, totalTurnaroundTime = 0;
-//        PriorityQueue<Process> readyQueue = new PriorityQueue<>(Comparator.comparingInt(p -> p.remainingTime));
-//
-//        while (completed < processes.size()) {
-//            for (Process p : processes) {
-//                if (p.arrivalTime <= currentTime && p.remainingTime > 0 && !readyQueue.contains(p)) {
-//                    readyQueue.add(p);
-//                }
-//            }
-//
-//            if (readyQueue.isEmpty()) {
-//                currentTime++;
-//                continue;
-//            }
-//
-//            Process currentProcess = readyQueue.poll();
-//            currentTime++;
-//            currentProcess.remainingTime--;
-//
-//            if (currentProcess.remainingTime == 0) {
-//                completed++;
-//                currentProcess.turnaroundTime = currentTime - currentProcess.arrivalTime;
-//                currentProcess.waitingTime = currentProcess.turnaroundTime - currentProcess.burstTime;
-//                totalWaitingTime += currentProcess.waitingTime;
-//                totalTurnaroundTime += currentProcess.turnaroundTime;
-//
-//                System.out.println("Process " + currentProcess.name + ": Waiting Time = " + currentProcess.waitingTime
-//                        + ", Turnaround Time = " + currentProcess.turnaroundTime);
-//
-//                currentTime += contextSwitchTime;
-//            }
-//        }
-//
-//        System.out.println("Average Waiting Time = " + (double) totalWaitingTime / processes.size());
-//        System.out.println("Average Turnaround Time = " + (double) totalTurnaroundTime / processes.size());
-//    }
-
-    // FCAI Scheduling
-//    static void simulateFCAIScheduling(List<Process> processes, int quantumTime, int contextSwitchTime) {
-//        System.out.println("\n=== FCAI Scheduling ===");
-//        int currentTime = 0, completed = 0, totalWaitingTime = 0, totalTurnaroundTime = 0;
-//
-//        double v1 = (double) processes.stream().mapToInt(p -> p.getArrivalTime()).max().orElse(1) / 10;
-//        double v2 = (double) processes.stream().mapToInt(p -> p.getBurstTime()).max().orElse(1) / 10;
-//
-//        PriorityQueue<Process> readyQueue = new PriorityQueue<>(Comparator.comparingDouble(
-//                p -> (10 - p.getPriority()) + (p.getArrivalTime() / v1) + (p.getRemainingTime() / v2)));
-//
-//        while (completed < processes.size()) {
-//            for (Process p : processes) {
-//                if (p.getArrivalTime() <= currentTime && p.getRemainingTime() > 0 && !readyQueue.contains(p)) {
-//                    readyQueue.add(p);
-//                }
-//            }
-//
-//            if (readyQueue.isEmpty()) {
-//                currentTime++;
-//                continue;
-//            }
-//
-//            Process currentProcess = readyQueue.poll();
-//            int executionTime = Math.min((int) Math.ceil(quantumTime * 0.4), currentProcess.getRemainingTime());
-//            currentTime += executionTime;
-//            currentProcess.setRemainingTime(currentProcess.getRemainingTime() - executionTime);
-//
-//            if (currentProcess.getRemainingTime() == 0) {
-//                completed++;
-//                currentProcess.setTurnaroundTime(currentTime - currentProcess.getArrivalTime());
-//                currentProcess.setWaitingTime(currentProcess.getTurnaroundTime() - currentProcess.getBurstTime());
-//                totalWaitingTime += currentProcess.getWaitingTime();
-//                totalTurnaroundTime += currentProcess.getTurnaroundTime();
-//
-//                System.out.println("Process " + currentProcess.name + ": Waiting Time = " + currentProcess.getWaitingTime()
-//                        + ", Turnaround Time = " + currentProcess.getTurnaroundTime());
-//
-//                currentTime += contextSwitchTime;
-//            } else {
-//                readyQueue.add(currentProcess);
-//            }
-//        }
-//
-//        System.out.println("Average Waiting Time = " + (double) totalWaitingTime / processes.size());
-//        System.out.println("Average Turnaround Time = " + (double) totalTurnaroundTime / processes.size());
-//    }
